6a2313d4e154716e57675f299a90bed97785c76a
Adam Waselnuk <adam.waselnuk@gmail.com>
2016-12-23 12:16:07 -0500

    Implmented heightmap set and broke out some utils

--- HeightMap.elm
+++ HeightMap.elm
@@ -29,16 +29,39 @@ build exponent =
 get : HeightMap -> Coord -> Maybe Float
 get heightMap coord =
   let
-    (x, y) = coord
-    index = heightMap.resolution * x + y
+    index = toIndex heightMap coord
   in
-    if (x <= heightMap.last && y<= heightMap.last) then
+    if (inBounds heightMap coord) then
       Array.get index heightMap.cells
     else
       Maybe.Nothing
 
 -- Set a value at x, y and return an error if the coord is out of bounds
--- set : HeightMap -> Coord -> Float -> Float
+set : HeightMap -> Coord -> Float -> HeightMap
+set heightMap coord val =
+  let
+    index = toIndex heightMap coord
+  in
+    if (inBounds heightMap coord) then
+      { heightMap | cells = Array.set index val heightMap.cells }
+    else
+      heightMap
+
+-- Check if coord is in bounds
+inBounds : HeightMap -> Coord -> Bool
+inBounds heightMap coord =
+  let
+    (x, y) = coord
+  in
+    x <= heightMap.last && y <= heightMap.last
+
+-- Convert coord to array index
+toIndex : HeightMap -> Coord -> Int
+toIndex heightMap coord =
+  let
+    (x, y) = coord
+  in
+    heightMap.resolution * x + y
 
 -- Normalize the heightmap, no value should be below 0.0 or above 1.0
 -- normalize : HeightMap -> Int -> Int -> HeightMap
